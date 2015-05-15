a0bb3a988768f2dc040d4ce2d8857c36a4440ecb
Marcin Badurowicz <m@badurowicz.net>
2015-05-15 08:44:59 +0200

    powrót AssemblyInfo.cs do repozytorium

---
+++ DjToKey/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+
+
+[assembly: System.Reflection.AssemblyTitle("DjToKey")]
+[assembly: System.Reflection.AssemblyDescription("DjToKey")]
+[assembly: System.Reflection.AssemblyCompany("Marcin Badurowicz")]
+[assembly: System.Reflection.AssemblyProduct("DjToKey")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright © Marcin Badurowicz 2015")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.CLSCompliant(false)]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
