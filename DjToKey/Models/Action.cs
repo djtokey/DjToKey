e972d26eb64f66528ea2b580ebe82db44f51da6a
Marcin Badurowicz <m@badurowicz.net>
2015-05-15 09:23:49 +0200

    inny sposób przekazywania obiektów do skryptu

--- DjToKey/Models/Action.cs
+++ DjToKey/Models/Action.cs
@@ -14,15 +14,12 @@ namespace DjToKey.Models
 
         public void Execute(int val, V8ScriptEngine eng)
         {
-            string command = Command.Trim();
-            command = command.Replace("{VAL}", val.ToString());
-            command = command.Replace("{VALD}", (val == 127)? "1" : "-1");
-
             eng.AddHostObject("Keyboard", Simulator.Input.Keyboard);
             eng.AddHostObject("Mouse", Simulator.Input.Mouse);
-            eng.AddHostObject("value", val);
+            eng.AddHostObject("val", val);
+            eng.AddHostObject("Value", new { Raw = val, Transformed = (val == 127)? 1 : -1 });
 
-            eng.Execute(command);            
+            eng.Execute(this.Command);            
         }
     }
 }
