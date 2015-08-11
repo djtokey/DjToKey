68cb086f6063fb35f4712a1684de38e5803fec4e
Marcin Badurowicz <m@badurowicz.net>
2015-08-12 00:11:10 +0200

    small name changes

--- PluginContracts/Properties/AssemblyInfo.cs
+++ PluginContracts/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: System.Reflection.AssemblyTitle("Ktos.DjToKey.PluginContracts")]
+[assembly: System.Reflection.AssemblyTitle("Ktos.DjToKey.Plugins.Contracts")]
 [assembly: System.Reflection.AssemblyDescription("DjToKey Plugins Contracts")]
 [assembly: System.Reflection.AssemblyCompany("Marcin Badurowicz")]
 [assembly: System.Reflection.AssemblyProduct("DjToKey")]
