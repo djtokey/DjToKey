86ec3f1fb2b3c8638a4356fe2463ec6cd1c4e6f0
Marcin Badurowicz <m@badurowicz.net>
2015-10-04 00:13:59 +0200

    tray icon support

--- DjToKey/Views/MainWindow.xaml.cs
+++ DjToKey/Views/MainWindow.xaml.cs
@@ -39,6 +39,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -52,9 +53,37 @@ namespace Ktos.DjToKey.Views
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        NotifyIcon trayIcon;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            trayIcon = new NotifyIcon();
+            trayIcon.BalloonTipIcon = ToolTipIcon.Info;
+
+            trayIcon.Icon = DjToKey.Resources.AppResources.icon;
+            trayIcon.Text = DjToKey.Resources.AppResources.AppName;
+            trayIcon.DoubleClick += TrayIcon_DoubleClick;
+        }
+
+        private void TrayIcon_DoubleClick(object sender, EventArgs e)
+        {
+            this.WindowState = WindowState.Normal;
+        }
+
+        private void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+                trayIcon.Visible = true;
+                this.ShowInTaskbar = false;
+            }
+            else
+            {
+                trayIcon.Visible = false;
+                this.ShowInTaskbar = true;
+            }
         }
     }
 }
