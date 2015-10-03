94b3abe9725b72cb37fcbc46e9b436801a3c769c
Marcin Badurowicz <m@badurowicz.net>
2015-10-03 22:27:14 +0200

    moving project to WPF
    As well as upgrading some packages, installing MahApps.Metro and MahApps.Metro.Resources

--- DjToKey/Resources/AppResources.Designer.cs
+++ DjToKey/Resources/AppResources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Ktos.DjToKey.Resources {
+namespace DjToKey_WPF.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Ktos.DjToKey.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Ktos.DjToKey.Resources.AppResources", typeof(AppResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DjToKey_WPF.Resources.AppResources", typeof(AppResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
