4c4aa549132a80f35b759b4394ec46aacce612ef
Marcin Badurowicz <m@badurowicz.net>
2015-11-20 14:13:29 +0100

    mock MainWindow's ViewModel for design mode

---
+++ DjToKey/Mock/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Ktos.DjToKey.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ktos.DjToKey.Mock
+{
+    class MainWindowViewModel
+    {
+        /// <summary>
+        /// List of all devices supported by the application
+        /// </summary>
+        public ObservableCollection<Device> Devices { get; set; }
+
+        /// <summary>
+        /// Current device
+        /// </summary>
+        public Device CurrentDevice { get; set; }
+    }
+}
